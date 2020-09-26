641b12a91574c1c7b864cf320cb48dc7ea8250c7
Antonio Ivanov <starshine1215@gmail.com>
2020-09-26 14:02:46 +0300

    замена адреса апи

--- src/index.js
+++ src/index.js
@@ -14,55 +14,45 @@ import {
   RESULTS__LOADER,
   SHOW_MORE,
   NEWSAPI_TOKEN,
-  ERROR_IMAGE
+  ERROR_IMAGE,
 } from "./js/constants/constants";
 
 import DataStorage from "./js/modules/datastorage";
-import NewsCardList from "./js/components/newscardlist"
-import NewsCard from "./js/components/newscard"
+import NewsCardList from "./js/components/newscardlist";
+import NewsCard from "./js/components/newscard";
 
-(function () {
-  const NEWS_API = new NewsApi(
-    INPUT, 
-    NEWSAPI_TOKEN
-  );
+const NEWS_API = new NewsApi(INPUT, NEWSAPI_TOKEN);
 
-  const NEWS_CARD = new NewsCard();
-  
-  const NEWS_CARD_LIST = new NewsCardList(
-    RESULTS,
-    RESULTS_NEWS_LIST,
-    STORAGE_DATA,
-    NEWS_CARD,
-    NEWS_API,
-    RESULTS__ERROR,
-    RESULTS__NORESULTS,
-    RESULTS__NEWS,
-    RESULTS__LOADER,
-    SHOW_MORE,
-    INPUT,
-    ERROR_IMAGE
-  ); 
+const NEWS_CARD = new NewsCard();
 
-  const DATA_STORAGE = new DataStorage(
-    RESULTS,
-    NEWS_CARD_LIST,
-    STORAGE_DATA
-  );
-  DATA_STORAGE.getLastResults();
+const NEWS_CARD_LIST = new NewsCardList(
+  RESULTS,
+  RESULTS_NEWS_LIST,
+  STORAGE_DATA,
+  NEWS_CARD,
+  NEWS_API,
+  RESULTS__ERROR,
+  RESULTS__NORESULTS,
+  RESULTS__NEWS,
+  RESULTS__LOADER,
+  SHOW_MORE,
+  INPUT,
+  ERROR_IMAGE
+);
 
-  SEARCH_FIELD.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (event.key === "Enter") {
-      NEWS_CARD_LIST.getNews();
-    }
+const DATA_STORAGE = new DataStorage(RESULTS, NEWS_CARD_LIST, STORAGE_DATA);
+DATA_STORAGE.getLastResults();
+
+SEARCH_FIELD.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (event.key === "Enter") {
     NEWS_CARD_LIST.getNews();
-  })
+  }
+  NEWS_CARD_LIST.getNews();
+});
 
-  SHOW_MORE.addEventListener("click", () => NEWS_CARD_LIST.showMoreNews(JSON.parse(localStorage.getItem(`${STORAGE_DATA}`))));
-})();
-
-
-
-
-    
+SHOW_MORE.addEventListener("click", () =>
+  NEWS_CARD_LIST.showMoreNews(
+    JSON.parse(localStorage.getItem(`${STORAGE_DATA}`))
+  )
+);
